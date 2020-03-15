cb76c10aeec0e32209857ab36f28f7e3934d244a
flyingsl0ths <randomemail32110@gmail.com>
2020-03-15 13:41:14 -0600

    Updated file

--- Open File Dialog/parttwelve.py
+++ Open File Dialog/parttwelve.py
@@ -9,13 +9,23 @@ root.title("Window 1")
 # an initial directory or a directory
 # specified by tuples of file type and file extension pair
 # in filetypes
-root.filename = filedialog.askopenfile(
-    initialdir="./res", title="Select A File", filetypes=(("png files", "*png"), ("jpg file", "*jpg"), ("all files", "*.*")))
 
-# Example of using the image file object returned from filedialog
-# an displaying it within root window
-my_img = ImageTk.PhotoImage(Image.open(root.filename.name))
-my_lbl = Label(root, image=my_img)
-my_lbl.pack()
+my_img = 0
+
+
+def open_img():
+    global my_img
+    # Example of using the pointer to an image file object
+    # returned from filedialog an displaying it within root window
+    root.filename = filedialog.askopenfile(
+        initialdir="./res", title="Select A File", filetypes=(("png files", "*png"), ("jpg file", "*jpg"), ("all files", "*.*")))
+
+    my_img = ImageTk.PhotoImage(Image.open(root.filename.name))
+    my_lbl = Label(root, image=my_img)
+    my_lbl.pack()
+
+
+my_button = Button(root, text="open image", command=open_img)
+my_button.pack()
 
 mainloop()
